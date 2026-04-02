0982f883752fb3c1bb8c2d1c13e252b8049d1859
Ayushi Chouhan <kavita04chouhan@gmail.com>
2026-04-02 15:36:31 +0530

    Add files via upload

---
+++ src/Components/Barcode_Solution.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import icon1 from '../assets/bricon1.png'
+
+function Barcode_Solution() {
+  return (
+    <>
+    <div className='barcode-solution'>
+        <div className='barcode-overlay'>
+            <h3>A Complete Barcode Solution for Industries</h3>
+            <div className='barcode-icons'>
+               <div className='barcode-icon-inner'>
+                <div>
+                <div className='icons'><img src={icon1} alt='image1'></img></div>
+               </div>
+               <h4>Retails</h4>
+               </div>
+                <div className='barcode-icon-inner'>
+                <div>
+                <div className='icons'><img src={icon1} alt='image2'></img></div>
+               </div>
+               <h4>Retails</h4>
+               </div>
+                <div className='barcode-icon-inner'>
+                <div>
+                <div className='icons'><img src={icon1} alt='image3'></img></div>
+               </div>
+               <h4>Retails</h4>
+               </div>
+                <div className='barcode-icon-inner'>
+                <div>
+                <div className='icons'><img src={icon1} alt='image4'></img></div>
+               </div>
+               <h4>Retails</h4>
+               </div>
+                <div className='barcode-icon-inner'>
+                <div>
+                <div className='icons'><img src={icon1} alt='image5'></img></div>
+               </div>
+               <h4>Retails</h4>
+               </div>
+                <div className='barcode-icon-inner'>
+                <div>
+                <div className='icons'><img src={icon1} alt='image6'></img></div>
+               </div>
+               <h4>Retails</h4>
+               </div>
+            </div>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Barcode_Solution
